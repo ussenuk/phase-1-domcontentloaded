792bff0ecddc75ee8e7b078a2f7530ccce2da7d4
Ussen_Kimanuka <ukimanuka@gmail.com>
2023-10-22 17:31:23 +0300

    manipulating dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+
+    const p = document.getElementById("text");
+    p.textContent = "This is really cool!";
+
+  });
+  
